b7a28cf138796357bad0905dd45c6cf9983ee3a7
Mark Tuddenham <mark@tudders.com>
2020-01-03 16:43:37 +0000

    use purgecss not on prod

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,8 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
   // Specify the paths to all of the template files in your project 
   content: [
-    './build/**/*.html',
+    './**/*.html',
+    'popup.js',
   ],
 
   // Include any special characters you're using in this regular expression
@@ -13,8 +14,9 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...process.env.NODE_ENV === 'production'
-      ? [purgecss]
-      : []
+    purgecss
+      // ...process.env.NODE_ENV === 'production'
+    //   ? [purgecss]
+    //   : []
   ]
 }
